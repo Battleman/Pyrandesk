65e6ed1708ed9aa46dfe9b844c10826daf0db906
Olivier Cloux <olivier.cloux@epfl.ch>
2018-06-16 17:48:51 +0200

    Added offline support, cache support and general cleaning

--- random_imgur_desktop.py
+++ random_imgur_desktop.py
@@ -3,13 +3,16 @@
 This is a good code
 """
 import argparse
+import fnmatch
 import getpass
+import os
 import random
 import re
+import stat
+import subprocess
 import sys
 import time
 from io import BytesIO
-from os.path import isfile
 from pprint import pprint
 
 import requests
@@ -41,7 +44,13 @@ def download_from_url(link):
     return req.content
 
 
-def get_extension_from_json(json):
+def get_mimetype_from_json(json):
+    """
+    Determines extension (jpeg, png,...) file should have
+
+    This information exists in the metadata. We parse it to obtain the
+    information
+    """
     try:
         # extension should be 'image/jpeg' or 'image/png' or...
         extension = json['type'].split('/')[1]
@@ -60,8 +69,9 @@ class RandomImgurDesktop():
     def __init__(self):
         self.albums = []
         self.image_counter = 0
+        self.sfw = False
         config_file = 'config.yaml'
-        if isfile(config_file):
+        if os.path.isfile(config_file):
             with open(config_file) as file:
                 config = yaml.load(file)
         else:
@@ -74,7 +84,9 @@ class RandomImgurDesktop():
         self.cache_size = config['default_cache_size']
         self.cache_dir = config['default_cache_dir'].replace(
             '<USERNAME>', user)
-        pprint(self.cache_dir)
+
+        if self.cache_size > 0:
+            self.init_cache()
 
     def get_json(self, album_hash):
         """
@@ -87,7 +99,7 @@ class RandomImgurDesktop():
             params=payload).json()
 
         if response['success']:
-            print("Data was successfully loaded")
+            # print("Data was successfully loaded")
             return response['data']
 
         print(
@@ -110,6 +122,65 @@ class RandomImgurDesktop():
         some_index = random.randint(0, len(self.albums)-1)
         return self.albums[some_index]
 
+    def init_cache(self):
+        """
+        Ensures the cache is not too full at startup
+
+        Checks there are less files in cache than specified size.
+        If too many, removes oldest ones to have fitting size.
+        """
+        file_list = []
+        for filename in os.listdir(self.cache_dir):
+            if fnmatch.fnmatch(filename, 'image_*'):
+                absolute = self.cache_dir+filename
+                file_list.append((os.stat(absolute)[stat.ST_MTIME], absolute))
+        file_list.sort(key=lambda a: a[0])
+        to_delete = len(file_list)-self.cache_size
+        if to_delete > 0:
+            for file in file_list[-to_delete:]:
+                print("Cache too full, cleaning file {}".format(file))
+                os.remove(file)
+
+    def update_background(self, filename):
+        """
+        Use dconf to update the background
+        """
+        key = "/org/gnome/desktop/background/picture-uri"
+        value = "'file://"+filename+"'"
+        # print("Calling", "dconf", "write", key, value)
+        subprocess.Popen([
+            "dconf", "write", key, value
+        ])
+
+        ######
+        # personal need for my conky.
+        # You can probably safely delete this.
+        subprocess.Popen([
+            "feh", "--bg-fill", filename
+        ])
+        #####
+
+        print("Background modified")
+
+    def no_internet(self):
+        """
+        Called when internet is not activated or imgur is unreachable
+
+        This method will basically comport as the original version, but will
+        only use cached images
+        """
+        file_list = []
+        for filename in os.listdir(self.cache_dir):
+            if fnmatch.fnmatch(filename, 'image_*'):
+                absolute = self.cache_dir+filename
+                file_list.append(absolute)
+        print(file_list)
+        if len(file_list) < 0:
+            return False
+        random_cached_file = file_list[random.randint(0, len(file_list)-1)]
+        self.update_background(random_cached_file)
+        return True
+
     def download_random_image(self, album_data):
         """
         From a data response (list of json), downloads one image at random
@@ -126,11 +197,14 @@ class RandomImgurDesktop():
             0,
             len(album_data['images'])-1)]
         image = download_from_url(random_image_metadata['link'])
-        extension = get_extension_from_json(random_image_metadata)
+        extension = get_mimetype_from_json(random_image_metadata)
         filename = self.save_image(image, extension)
         return filename
 
     def save_image(self, binary, extension):
+        """
+        Save an image with given extension to the cache directory of the object
+        """
         i = Image.open(BytesIO(binary))
         filename = self.cache_dir + "image_{:05}.{}".format(
             self.image_counter, extension)
@@ -144,8 +218,13 @@ class RandomImgurDesktop():
             print("Permission to write at location {}".format(filename) +
                   "was denied, ensure you have correct rights.")
             return False
-        
+
         self.image_counter += 1
+        if self.image_counter == self.cache_size:
+            print("Reached max cache size ({})".format(self.cache_size) +
+                  ", starting over")
+            self.image_counter = 0
+
         return filename
 
     def add_album(self, album):
@@ -158,14 +237,14 @@ class RandomImgurDesktop():
             self.albums.append(album)
             return True
         # whole URL
-        regex = re.compile(r"^http.?://imgur.com/a/(\w+)$")
+        regex = re.compile(r"^https://imgur.com/a/(\w+)\s*$")
         if regex.match(album):
             album = regex.sub(r"\1", album)
-            print(album)
+            # print(album)
             self.albums.append(album)
             return True
 
-        print("This url album is not valid")
+        print("This url album is not valid:", album)
         return False
 
     def add_multiple_albums(self, filename):
@@ -174,11 +253,12 @@ class RandomImgurDesktop():
         to the directory of albums. Lets add_album handle the parsing and
         checking
         """
-        if isfile(filename):
-            with open(filename) as file:
+        if os.path.isfile(filename):
+            with open(filename, "r") as file:
                 for line in file.readlines():
+                    print("Trying to add line", line.replace('\n', ''))
                     self.add_album(line)
-                    return True
+            return True
         print("Non existing file \""+filename+"\"")
         return False
 
@@ -187,7 +267,7 @@ def main():
     """
     Main of the script
     """
-    sleeptime = 20
+    sleeptime = 5
     random_imgur_desktop = RandomImgurDesktop()
 
     ###
@@ -208,10 +288,10 @@ def main():
     parser.add_argument(
         "-c", "--cache-size",
         help="Specify the number of images to\
-            keep in cache. Default to 10")
+            keep in cache. Default to 10. 0 means no limit")
 
     parser.add_argument(
-        "-s", "--safe-for-work",
+        "-s", "--safe-for-work", action="store_true",
         help="Don't download pictures marked as NSFW ('not safe wor work',\
         that is images with potentially adult content)"
     )
@@ -232,34 +312,62 @@ def main():
     elif args.galleries:
         galleries = args.galleries
         if "../" in galleries:
-            print("No relative path in galleries !")
+            print("No relative path in file containing albums!")
+            return -1
+        if not random_imgur_desktop.add_multiple_albums(galleries):
+            print("Impossible to read file. Exiting.")
             return -1
     else:
-        print("You need to specify at least one album")
+        print("You need to specify at least one album (with -a or -A)")
         return -1
+
     if args.cache_size:
         cache_size = args.cache_size
         try:
             cache_size = int(cache_size)
         except ValueError:
-            print("Cache size must be an integer")
-            return -1
-        if cache_size < 0:
-            print("Cache size must be greater or equal to 0")
+            print("Cache size must be an integer. Using default value.")
+        else:
+            if cache_size < 0:
+                print("Cache size must be greater or equal to 0.\
+                Using default value")
+            else:
+                random_imgur_desktop.cache_size = cache_size
+
     if args.cache_directory:
         galleries = args.galleries
         if "../" in galleries:
             print("No relative path in galleries !")
             return -1
 
+    random_imgur_desktop.sfw = args.safe_for_work
+
+    print(random_imgur_desktop.sfw)
     while True:
-        album = random_imgur_desktop.random_select_album()
-        # album = hash ≃ k3AMe
-        json = random_imgur_desktop.get_json(album)
-        # json = album metadata + list of images metadata
-        image = random_imgur_desktop.download_random_image(json)
-        print(image)
-        time.sleep(sleeptime)
+        try:
+            conn = requests.head("http://www.imgur.com")
+        except requests.exceptions.ConnectionError:
+            # no internet connection
+            print("No internet :( trying to use local cache...")
+            if not random_imgur_desktop.no_internet():
+                print("No internet and cache is empty, quitting")
+                return -1
+        else:
+            if conn.ok:
+                print("Connection ok, going normal")
+                album_url = random_imgur_desktop.random_select_album()
+                album_json = random_imgur_desktop.get_json(album_url)
+                image_filename = random_imgur_desktop.download_random_image(
+                    album_json)
+                random_imgur_desktop.update_background(image_filename)
+            else:
+                # imgur is not accessible
+                print("Imgur not accessible ? trying to use local cache...")
+                if not random_imgur_desktop.no_internet():
+                    print("Imgur not accessible and cache is empty, quitting")
+                    return -1
+        finally:
+            time.sleep(sleeptime)
     return 0
 
 
